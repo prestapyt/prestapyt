ccefd24e4add946528f7071c22be75ec92efdb56
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2012-08-23 16:48:09 +0200

    fix setup.py indentation

--- setup.py
+++ setup.py
@@ -10,31 +10,31 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-	# Basic package information.
-	name = 'prestapyt',
-	version = __version__,
+    # Basic package information.
+    name = 'prestapyt',
+    version = __version__,
 
-	# Packaging options.
-	include_package_data = True,
+    # Packaging options.
+    include_package_data = True,
 
-	# Package dependencies.
-	install_requires = ['httplib2',],
+    # Package dependencies.
+    install_requires = ['httplib2',],
 
-	# Metadata for PyPI.
-	author = 'Guewen Baconnier',
-	author_email = 'guewen.baconnier@gmail.com',
-	license = 'GNU AGPL-3',
-	url = 'http://github.com/guewen/prestapyt',
+    # Metadata for PyPI.
+    author = 'Guewen Baconnier',
+    author_email = 'guewen.baconnier@gmail.com',
+    license = 'GNU AGPL-3',
+    url = 'http://github.com/guewen/prestapyt',
     packages=['prestapyt'],
-	keywords = 'prestashop api client rest',
-	description = 'A library to access Prestashop Web Service from Python.',
-	long_description = read('README'),
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Intended Audience :: Developers',
-		'License :: OSI Approved :: GNU Affero General Public License v3',
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Topic :: Internet :: WWW/HTTP :: Site Management',
-		'Topic :: Internet'
-	]
+    keywords = 'prestashop api client rest',
+    description = 'A library to access Prestashop Web Service from Python.',
+    long_description = read('README'),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Internet'
+        ]
 )
