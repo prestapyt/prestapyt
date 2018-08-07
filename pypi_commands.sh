0bd49237c0f08dc03be8af765e25e1a5d677096c
Quentin THEURET <quentin@quentin-theuret.net>
2018-08-07 13:52:19 +0200

    [IMP] Deploy on both Python versions

--- pypi_commands.sh
+++ pypi_commands.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+python2.7 setup.py sdist upload
+python2.7 setup.py bdist_egg upload
+python2.7 setup.py bdist_wheel upload
 python3 setup.py sdist upload
 python3 setup.py bdist_egg upload
 python3 setup.py bdist_wheel upload
