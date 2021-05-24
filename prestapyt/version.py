72dbe21c32d10fe0cf39b4bbe9d7582582365b47
omalbastin <omalbastin@gmail.com>
2021-05-24 17:20:38 +0530

    Added package dependency importlib-metadata for python <3.8

--- prestapyt/version.py
+++ prestapyt/version.py
@@ -1,6 +1,7 @@
 try:
     from importlib.metadata import version, PackageNotFoundError
 except:
+    # for python < 3.8
     from importlib_metadata import version, PackageNotFoundError
 
 __author__ = "Guewen Baconnier <guewen.baconnier@gmail.com>"
